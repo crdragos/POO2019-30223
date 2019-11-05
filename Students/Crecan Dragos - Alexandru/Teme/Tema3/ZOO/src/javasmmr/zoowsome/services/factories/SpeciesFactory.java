23180b0d861889ccfcbfc9cb49a30d9ab09fcacb
Crecan Dragos - Alexandru <dcrecan@icloud.com>
2019-11-05 23:39:23 +0200

    stergere tema 3

--- Students/Crecan Dragos - Alexandru/Teme/Tema3/ZOO/src/javasmmr/zoowsome/services/factories/SpeciesFactory.java
+++
@@ -1,6 +0,0 @@
-package javasmmr.zoowsome.services.factories;
-import javasmmr.zoowsome.models.animals.Animal;
-
-public abstract class SpeciesFactory {
-    public abstract Animal getAnimal(String type);
-}
